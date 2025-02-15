c09a7a5a4074a6ab2560a1224da9c3a1d9f5bf2d
Sarah Haddix <sarahbh@unc.edu>
2025-02-15 13:30:19 -0500

    pictures

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,9 +1,10 @@
 const images = [
   "/assets/images/image1.jpg",
   "/assets/images/image2.jpg",
-  "/assets/images/image3.jpg",
-  "/assets/images/image4.jpg",
+  //"/assets/images/image3.jpg",
+  //"/assets/images/image4.jpg",
   "/assets/images/image5.jpg"
+  "/assets/images/image6.jpg"
 ];
 
 // Get the image element
