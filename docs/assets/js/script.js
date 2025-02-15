98b7955a3b1dbb8539e5f8f5b9094870a76abec6
Sarah Haddix <sarahbh@unc.edu>
2025-02-15 13:33:03 -0500

    pictures fixed bug

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -3,7 +3,7 @@ const images = [
   "/assets/images/image2.jpg",
   //"/assets/images/image3.jpg",
   //"/assets/images/image4.jpg",
-  "/assets/images/image5.jpg"
+  "/assets/images/image5.jpg",
   "/assets/images/image6.jpg"
 ];
 
