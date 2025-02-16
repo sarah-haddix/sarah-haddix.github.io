aa31919c79d8b2dd16c0f009eef84beec2f00f2c
Sarah Haddix <sarahbh@unc.edu>
2025-02-16 17:36:08 -0500

    changed picture order

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,10 +1,10 @@
 const images = [
+  "/assets/images/image6.jpg",
   "/assets/images/image1.jpg",
-  "/assets/images/image2.jpg",
   //"/assets/images/image3.jpg",
   //"/assets/images/image4.jpg",
-  "/assets/images/image5.jpg",
-  "/assets/images/image6.jpg"
+  "/assets/images/image2.jpg",
+  "/assets/images/image5.jpg"
 ];
 
 // Get the image element
