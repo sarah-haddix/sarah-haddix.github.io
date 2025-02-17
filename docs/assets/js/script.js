ecf3ed32078b9ccf5223c237a2d5237f95004e71
Sarah Haddix <sarahbh@unc.edu>
2025-02-17 09:20:38 -0500

    photo order

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,8 +1,6 @@
 const images = [
-  "/assets/images/image6.jpg",
   "/assets/images/image1.jpg",
-  //"/assets/images/image3.jpg",
-  //"/assets/images/image4.jpg",
+  "/assets/images/image6.jpg",
   "/assets/images/image2.jpg",
   "/assets/images/image5.jpg"
 ];
